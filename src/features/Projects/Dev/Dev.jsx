60383dabb5eed142866315227d15daf6a9edb4d9
MiguelMartini <miguelkaulingm21@gmail.com>
2026-01-24 00:33:16 -0300

    Feat: Development Section

---
+++ src/features/Projects/Dev/Dev.jsx
@@ -0,0 +1,53 @@
+import DevIcon from "@/components/DevIcon";
+import React from "react";
+
+function Dev({ title, text, techs, techIcons }) {
+  return (
+    <div
+      className="bg-[#374B22] text-[#EDECED] py-10 px-6 
+    lg:px-30 lg:py-10"
+    >
+      <p className="text-2xl font-bold lg:text-4xl">{title}</p>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
+        <div className="my-4">
+          <p
+            className="text-[#EDECED]
+           text-base
+           leading-relaxed
+           text-justify
+           indent-6
+           lg:text-xl
+           lg:leading-loose"
+          >
+            {text}
+          </p>
+          <div className="border-t-2 border-[#EDECED] mt-4 pt-4"></div>
+          <p
+            className="text-[#EDECED]
+           text-base
+           leading-relaxed
+           text-justify
+           lg:text-xl
+           lg:leading-loose"
+          >
+            {techs}
+          </p>
+        </div>
+        <div>
+          <div className="p-10 bg-neutral-300 flex items-center flex-col gap-5 h-fit rounded-xl">
+            <p className="text-xl lg:text-2xl font-semibold text-[#374B22]">
+              Tecnologias
+            </p>
+            <div className="grid grid-cols-2 sm:flex sm:flex-row gap-2">
+              {techIcons.map((techIcon, index) => (
+                <DevIcon key={index} link={techIcon} alt={`icone-tecnlogia`} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dev;
--- src/pages/SenacFood.jsx
+++ src/pages/SenacFood.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Header from '../features/Layout/header/Header'
+import FooterSection from '../features/Layout/footer/FooterSection'
+import CarouselSection from '../features/Projects/Carousel/CarouselSection'
+import About from '@/features/Projects/ProjectAbout/About'
+
+import senacFood from "../assets/senacFood.png";
+import senacFood2 from "../assets/microsys.png";
+import Dev from '@/features/Projects/Dev/Dev'
+
+const imgs = [
+  senacFood,
+  senacFood2
+]
+
+const texts = [
+  "A proposta deste projeto será entregar um aplicativo de receitas culinárias, possibilitando ao usuário encontrar receitas adaptadas ao seu dia a dia, aos ingredientes que possui em casa e o seu nível de experiência.",
+
+  "O aplicativo contará com um design fluído, responsivo e funcional, com o objetivo de incentivar à diversificação alimentar e estímulo alimentar que é crucial para evitar carências nutricionais. Atendendo àqueles que procuram por praticidade ou quanto quem busca por novas experiências e deseja reduzir o desperdício alimentar.",
+  
+  "O uso de agentes de IA para sugerir receitas personalizadas baseadas nas receitas e ingredientes já cadastrados."
+]
+
+const techs = [
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/insomnia/insomnia-original.svg"
+]
 
 function SenacFood() {
   return (
-    <div>SenacFood</div>
+    <div className='bg-[#EDECED]'>
+      <Header/>
+      <CarouselSection title={"SenacFood"} imgs={imgs}/>
+      <About texts={texts}/>
+      <Dev title={"Desenvolvimento"} text={"Fui responsável por desenvolver o Backend da aplicação, onde fiz levantamentos de requisitos, modelo relacional de banco de dados, integração com modelos de inteligência Artificial e testes manuais da aplicação."} techs={"Tecnologias utilizadas: PHP, Laravel, MySQL, Git, GitHub, Insomnia"} techIcons={techs}/>
+      <FooterSection/>
+    </div>
   )
 }
 
